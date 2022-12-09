76641d5ba6d8bb0424c8cc2bce4f5e1e88215594
#lagonzalezdev <gonzalezortizlau@gmail.com>
2022-12-09 17:59:44 -0500

    search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 //components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
